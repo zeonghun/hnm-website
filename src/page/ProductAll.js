374fa273e4e64be0f6bba55d26bfcf19b52dc925
Jeonghun Kim <zeonghun413@gmail.com>
2023-01-26 16:08:10 +0900

    API call

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ProductAll = () => {
-  return <div>전체 상품 페이지</div>;
+  const getProducts = async () => {
+    // API 호출
+    let url = "http://localhost:5000/products";
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return <div></div>;
 };
 
 export default ProductAll;
